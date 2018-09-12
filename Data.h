0fb203b4c7e3c2af959622bf1854b3d4b33e7517
kyson1 <kyson1@users.noreply.github.com>
2018-09-12 15:09:11 -0400

    Added header file

---
+++ Data.h
@@ -0,0 +1,13 @@
+class Data {
+private:
+int x;
+float y;
+
+public:
+Data();
+void setY(float b)
+Data (int a, float b);
+int getX()
+float getY();
+void setX(int a);
+};
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "data.h"
+using namespace std;
+
+int maint() {
+vector <Data> v;
+
+v.push_back(7,21.01);
+Data d;
+v.push_back(d);
+d.setX(10);
+d.setY(12.01);
+v.push_back(d);
+
+for (int i=0; i< v.size(); i++) {
+cout << v[i].getX() << " ";
+cout << v[i].getY() << endl;
+
+}
