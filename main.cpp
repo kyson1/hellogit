ae9fc2940a70d5284ad7c2e38f980585e2b1cced
kyson1 <kyson1@user.noreply.com>
2018-09-26 15:17:45 -0400

    Lab 2 Pract 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "Data.h"
 using namespace std;
 
+
+void bubbleSort (auto& v);
+
 int main() {
 vector <Data> v;
 
@@ -13,8 +16,27 @@ d.setX(10);
 d.setY(12.0);
 v.push_back(d);
 
+
+bubbleSort(v);
+
+
 for (int i=0; i< v.size(); i++) {
 cout << v[i].getX() << " ";
-cout << v[i].getY() << endl;
+cout << v[i].getY() << " ";
+cout << endl;
+
 }
+}
+void bubbleSort (auto& v)
+
+{
+  cout<<endl<<"Bubble Sort"<<endl<<"---------------------"<<endl;
+
+  for ( int i = 0 ; i < v.size( ) ; i++)
+  {
+    for ( int j = 0 ; j < v.size () -1 ; j++)
+	if ( v[j +1 ].getX() < v[ j ].getX() )
+	    swap( v[ j +1 ] , v[ j ] );
+
+  }//end outer for (this brace is needed to include the print statement)
 }
